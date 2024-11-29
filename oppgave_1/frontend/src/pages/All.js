84a13245371aa1cb318944a840d35277a86723d5
Mullerino1 <martinemu03@gmail.com>
2024-11-29 17:28:56 +0100

    sign up

--- oppgave_1/frontend/src/pages/All.js
+++ oppgave_1/frontend/src/pages/All.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import SignUp from "./SignUpPage";
 import {
   categories,
   comments,
@@ -355,103 +356,103 @@ function Lesson() {
   );
 }
 
-function SignUp() {
-  const [success, setSuccess] = useState(false);
-  const [formError, setFormError] = useState(false);
-  const [fields, setFields] = useState({
-    name: "",
-    email: "",
-    admin: false,
-  });
-  const router = useRouter();
+// export default function SignUp() {
+//   const [success, setSuccess] = useState(false);
+//   const [formError, setFormError] = useState(false);
+//   const [fields, setFields] = useState({
+//     name: "",
+//     email: "",
+//     admin: false,
+//   });
+//   const router = useRouter();
 
-  const formIsValid = Object.values(fields).filter((val) => val?.length === 0);
+//   const formIsValid = Object.values(fields).filter((val) => val?.length === 0);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setFormError(false);
-    setSuccess(false);
-    if (formIsValid.length === 0) {
-      setSuccess(true);
-      setTimeout(() => {
-        router.push("/kurs");
-      }, 500);
-    } else {
-      setFormError(true);
-    }
-  };
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     setFormError(false);
+//     setSuccess(false);
+//     if (formIsValid.length === 0) {
+//       setSuccess(true);
+//       setTimeout(() => {
+//         router.push("/kurs");
+//       }, 500);
+//     } else {
+//       setFormError(true);
+//     }
+//   };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFields((prev) => ({ ...prev, [name]: value }));
-  };
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setFields((prev) => ({ ...prev, [name]: value }));
+//   };
 
-  return (
-    <section className="mx-auto max-w-xl" data-testid="sign_up">
-      <h2 className="mb-4 text-xl font-bold" data-testid="title">
-        Ny bruker
-      </h2>
-      <form data-testid="form" onSubmit={handleSubmit} noValidate>
-        <label className="mb-4 flex flex-col" htmlFor="name">
-          <span className="mb-1 font-semibold">Navn*</span>
-          <input
-            className="rounded"
-            data-testid="form_name"
-            type="text"
-            name="name"
-            id="name"
-            value={fields?.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label className="mb-4 flex flex-col" htmlFor="email">
-          <span className="mb-1 font-semibold">Epost*</span>
-          <input
-            className="rounded"
-            data-testid="form_email"
-            type="email"
-            name="email"
-            id="email"
-            value={fields?.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label className="flex items-center gap-2" htmlFor="admin">
-          <input
-            className="rounded"
-            data-testid="form_admin"
-            type="checkbox"
-            name="admin"
-            id="admin"
-            onChange={handleChange}
-            checked={fields?.admin}
-          />
-          <span className="font-semibold">Admin</span>
-        </label>
-        <button
-          className="mt-8 rounded bg-emerald-600 px-10 py-2 text-center text-base text-white"
-          data-testid="form_submit"
-          type="submit"
-        >
-          Lag ny bruker
-        </button>
-        {formError ? (
-          <p className="font-semibold text-red-500" data-testid="form_error">
-            Fyll ut alle felter med *
-          </p>
-        ) : null}
-        {success ? (
-          <p
-            className="font-semibold text-emerald-500"
-            data-testid="form_success"
-          >
-            Skjema sendt
-          </p>
-        ) : null}
-      </form>
-    </section>
-  );
-}
+//   return (
+//     <section className="mx-auto max-w-xl" data-testid="sign_up">
+//       <h2 className="mb-4 text-xl font-bold" data-testid="title">
+//         Ny bruker
+//       </h2>
+//       <form data-testid="form" onSubmit={handleSubmit} noValidate>
+//         <label className="mb-4 flex flex-col" htmlFor="name">
+//           <span className="mb-1 font-semibold">Navn*</span>
+//           <input
+//             className="rounded"
+//             data-testid="form_name"
+//             type="text"
+//             name="name"
+//             id="name"
+//             value={fields?.name}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label className="mb-4 flex flex-col" htmlFor="email">
+//           <span className="mb-1 font-semibold">Epost*</span>
+//           <input
+//             className="rounded"
+//             data-testid="form_email"
+//             type="email"
+//             name="email"
+//             id="email"
+//             value={fields?.email}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label className="flex items-center gap-2" htmlFor="admin">
+//           <input
+//             className="rounded"
+//             data-testid="form_admin"
+//             type="checkbox"
+//             name="admin"
+//             id="admin"
+//             onChange={handleChange}
+//             checked={fields?.admin}
+//           />
+//           <span className="font-semibold">Admin</span>
+//         </label>
+//         <button
+//           className="mt-8 rounded bg-emerald-600 px-10 py-2 text-center text-base text-white"
+//           data-testid="form_submit"
+//           type="submit"
+//         >
+//           Lag ny bruker
+//         </button>
+//         {formError ? (
+//           <p className="font-semibold text-red-500" data-testid="form_error">
+//             Fyll ut alle felter med *
+//           </p>
+//         ) : null}
+//         {success ? (
+//           <p
+//             className="font-semibold text-emerald-500"
+//             data-testid="form_success"
+//           >
+//             Skjema sendt
+//           </p>
+//         ) : null}
+//       </form>
+//     </section>
+//   );
+// }
 
 const isValid = (items) => {
   const invalidFields = [];
@@ -916,7 +917,7 @@ function Create() {
   );
 }
 
-export default function All() {
+ export default function All() {
   return (
     <div
       className="mx-auto grid min-h-screen w-full max-w-7xl grid-rows-[auto_minmax(900px,_1fr)_30px]"
@@ -937,6 +938,7 @@ export default function All() {
       </nav>
       <main className="h-full">
         <p>Siden er tom</p>
+        <SignUp/>
       </main>
       <footer className="flex justify-between" data-testid="footer">
         <p>Mikro LMS AS, 2024</p>
