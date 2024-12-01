a44c0d60df43664a693947f1609a163b36723d09
Mullerino1 <martinemu03@gmail.com>
2024-12-01 20:59:52 +0100

    eh

--- oppgave_2/frontend/src/Pages/events/[slug].tsx
+++ oppgave_2/frontend/src/Pages/events/[slug].tsx
@@ -7,6 +7,9 @@ import Layout from '@/layout/Layout';
 import { events, users } from '@/data/data';
 import "@/styles/css/main.css"
 import Arrangement from '../ArrangementPage';
+import Project from "@/components/Tickets";
+import useTicket from '@/hooks/useTickets';
+import type { HandleProject, Ticket as ProjectType } from "@/components/Types";
 
 //Slug setup was mainly found from this website https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes and form the class courses
 
@@ -31,6 +34,33 @@ export default function EventPage() {
   }
 
   const arrangementSlug = "";
+
+  const { add, status, get, data, error } = useTicket();
+  const tickets = data;
+
+  const handleProjectMutation: HandleProject = (props) => {
+    const { action } = props;
+
+    switch (action) {
+      case "add":
+        add(props.ticket);
+        break;
+    }
+  };
+
+  const addTicketServer = async (id: string) => {
+    try {
+      return fetch("http://localhost:4000", {
+        method: "POST",
+        body: JSON.stringify({
+          note: "",
+          projectId: id,
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
   
 
   return (
@@ -38,6 +68,7 @@ export default function EventPage() {
     <Layout>
 
     <div className="grid grid-cols-[250px_minmax(20%,1fr)_1fr] gap-16">
+      
     <aside className="border-r border-slate-200 pr-6">
      
                 <Link href={`/EventsPage`}>
@@ -58,13 +89,20 @@ export default function EventPage() {
               <h4>[{event.category}]</h4>
               <p>the price is {event.price}, we have {event.seats} seats</p>
               <p>{event.location}</p>
-              </section>
               <p
                 className="mt-4 font-semibold leading-relaxed"
                 data-testid="course_description"
               >
                 {event.description}
               </p>
+              </section>
+           
+              
+              <Project
+        tickets={tickets}
+        handleProjectMutation={handleProjectMutation}
+      >
+      </Project>
             </>
           </section>
         )}
