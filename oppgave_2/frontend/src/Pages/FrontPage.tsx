866bc6280725e0f47192236b9a6b4f2bfcb850f1
Mullerino1 <martinemu03@gmail.com>
2024-12-02 16:33:55 +0100

    cleaning

--- oppgave_2/frontend/src/Pages/FrontPage.tsx
+++ oppgave_2/frontend/src/Pages/FrontPage.tsx
@@ -3,48 +3,15 @@
 
 import React from "react";
 import Layout from "@/layout/Layout";
-import Project from "@/components/Tickets";
-import useTicket from "../hooks/useTickets"; 
-import type { HandleProject, Ticket as ProjectType } from "@/components/Types";
 
 export default function FrontPage() {
-  const { add, data } = useTicket();
-  const tickets = data;
-
-  const handleProjectMutation: HandleProject = (props) => {
-    const { action } = props;
-
-    switch (action) {
-      case "add":
-        add(props.ticket);
-        break;
-    }
-  };
-
-  const addTicketServer = async (id: string) => {
-    try {
-      return fetch("http://localhost:4000", {
-        method: "POST",
-        body: JSON.stringify({
-          note: "",
-          projectId: id,
-        }),
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
  
 
   return (
     <>
     <Layout>
-       <Project
-        tickets={tickets}
-        handleProjectMutation={handleProjectMutation}
-      >
-      </Project>
+       <h1>Do go to the events page to see the events :D</h1>
       </Layout>
     </>
   )
--- oppgave_2/frontend/src/types/Types.tsx
+++ oppgave_2/frontend/src/types/Types.tsx
@@ -1,38 +1,32 @@
-//need to set up a types file 
+// need to set up a types file 
 
-// import type { z } from "zod"
-// import type { projectSchema } from "../Features/Helpers/validate"
+import type { z } from "zod"
+// import type { ticketSchema } from "../Features/Helpers/validate"
+import type { ticketSchema } from "@/features/helpers/validate"
 
 
 
-// export const actions = {
-//   add: "add",
-//   remove: "remove",
-//   update: "update",
+export const actions = {
+  add: "add",
+  remove: "remove",
   
-// } as const
+} as const
 
-// export type HandleProjectProps =
-//   | {
-//       action: typeof actions.remove
-//       id: string
+export type HandleTicketProps =
+  | {
+      action: typeof actions.remove
+      id: string
+    }
+  | {
+      action: typeof actions.add
+      project: Partial<Ticket>
     
-//     }
-//   | {
-//       action: typeof actions.update
-//       id: string
-//       project: Partial<Project>
-//     }
-//   | {
-//       action: typeof actions.add
-//       project: Partial<Project>
-    
-//     }
+    }
 
-// export type HandleProject = (props: HandleProjectProps) => void
+export type HandleTicket = (props: HandleTicketProps) => void
 
-// export type Action = typeof actions
+export type Action = typeof actions
 
-// export type Project = z.infer<typeof projectSchema>
+export type Ticket = z.infer<typeof ticketSchema>
 
 
