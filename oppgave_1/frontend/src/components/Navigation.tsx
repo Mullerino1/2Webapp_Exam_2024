ada50d8e09c9c2544ef7968e088ccdd2117de7af
Mullerino1 <martinemu03@gmail.com>
2024-12-02 18:32:18 +0100

    cleaning

--- oppgave_1/frontend/src/components/Navigation.tsx
+++ oppgave_1/frontend/src/components/Navigation.tsx
@@ -16,32 +16,6 @@ export const Navigation = () => {
           </li>
         </ul>
       </nav>
-        // <nav className="mt-6 mb-12 flex justify-between">
-        //     <ul>
-        //         <li>
-        //         <h1 className="text-lg font-bold uppercase" data-testid="logo"><Link href="/">Home</Link></h1>
-        //         </li>
-        //         <li>
-        //         <h1 className="text-base font-semibold" data-testid="nav_courses"><Link href="Course">Course</Link></h1>
-        //         </li>
-        //         <li>
-        //         <h1 className="text-base font-semibold" data-testid="nav_new"><Link href="Create">Create</Link></h1>
-        //         </li>
-        //     </ul>
-        // </nav>
-    //     <nav className="mt-6 mb-12 flex justify-between">
-    //     <h1 className="text-lg font-bold uppercase" data-testid="logo">
-    //       <a href="/">Mikro LMS</a>
-    //     </h1>
-    //     <ul className="flex gap-8" data-testid="nav">
-    //       <li className="text-base font-semibold" data-testid="nav_courses">
-    //         <a href="kurs">Kurs</a>
-    //       </li>
-    //       <li className="text-base font-semibold" data-testid="nav_new">
-    //         <a href="/ny">Nytt kurs</a>
-    //       </li>
-    //     </ul>
-    //   </nav>
     )
 }
 
