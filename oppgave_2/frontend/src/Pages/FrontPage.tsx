8caa3d569a0ec01114da394ae0f7d31938d600b7
Mullerino1 <martinemu03@gmail.com>
2024-12-02 17:19:11 +0100

    slowww

--- oppgave_2/frontend/src/Pages/FrontPage.tsx
+++ oppgave_2/frontend/src/Pages/FrontPage.tsx
@@ -3,48 +3,13 @@
 
 import React from "react";
 import Layout from "@/layout/Layout";
-import Project from "@/components/Tickets";
-import useTicket from "../hooks/useTickets"; 
-import type { HandleProject, Ticket as ProjectType } from "@/components/Types";
 
 export default function FrontPage() {
-  const { add, status, get, data, error } = useTicket();
-  const tickets = data;
-
-  const handleProjectMutation: HandleProject = (props) => {
-    const { action } = props;
-
-    switch (action) {
-      case "add":
-        add(props.ticket);
-        break;
-    }
-  };
-
-  const addTicketServer = async (id: string) => {
-    try {
-      return fetch("http://localhost:4000", {
-        method: "POST",
-        body: JSON.stringify({
-          note: "",
-          projectId: id,
-        }),
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
- 
 
   return (
     <>
     <Layout>
-       <Project
-        tickets={tickets}
-        handleProjectMutation={handleProjectMutation}
-      >
-      </Project>
+       <h1>Go to the events page to see the most awesome events! :D</h1>
       </Layout>
     </>
   )
